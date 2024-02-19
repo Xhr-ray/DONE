a2aaec2c25e06080374191b5738b4944518916fe
Xhr-ray <62922855+Xhr-ray@users.noreply.github.com>
2024-02-19 22:06:16 +0800

    Add files via upload

---
+++ algorithms/optimizers/optimizer.py
@@ -0,0 +1,159 @@
+from torch import nn
+from torch.optim import Optimizer
+import numpy as np
+import torch
+
+
+class MySGD(Optimizer):
+    def __init__(self, params, lr):
+        defaults = dict(lr=lr)
+        super(MySGD, self).__init__(params, defaults)
+
+    def step(self, closure=None, hyper_learning_rate=0):
+        loss = None
+        if closure is not None:
+            loss = closure
+
+        for group in self.param_groups:
+            # print(group)
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                d_p = p.grad.data
+                if (hyper_learning_rate != 0):
+                    p.data.add_(-hyper_learning_rate, d_p)
+                else:
+                    p.data.add_(-group['lr'], d_p)
+        return loss
+
+
+class DANEOptimizer(Optimizer):
+    def __init__(self, params, lr=0.01, L = 0.1, eta = 1):
+        if lr < 0.0:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        defaults = dict(lr=lr, L=L, eta = eta)
+        super(DANEOptimizer, self).__init__(params, defaults)
+
+    def step(self, server_grads, pre_grads, pre_params, closure=None):
+        loss = None
+        if closure is not None:
+            loss = closure
+        for group in self.param_groups:
+            for p, server_grad, pre_grad, pre_param in zip(group['params'], server_grads, pre_grads, pre_params):
+                if server_grad.grad.data is not None and pre_grad.data is not None:
+                    p.data = p.data - group['lr'] * (p.grad.data - (pre_grad.data - group['eta'] * server_grad.grad.data) + group['L'] * (p.data - pre_param.data) + group['L'] * p.data )
+                else:
+                    p.data = p.data - group['lr'] * p.grad.data
+        return loss
+
+class Neumann(Optimizer):
+    """
+    Documentation about the algorithm
+    """
+
+    def __init__(self, params, lr=1e-3, eps=1e-8, alpha=1e-7, beta=1e-5, gamma=0.9, momentum=1, sgd_steps=5, K=10):
+
+        if not 0.0 <= lr:
+            raise ValueError("Invalid learning rate: {}".format(lr))
+        if not 0.0 <= eps:
+            raise ValueError("Invalid epsilon value: {}".format(eps))
+        if not 1 >= momentum:
+            raise ValueError("Invalid momentum value: {}".format(eps))
+
+        self.iter = 0
+        # self.sgd = SGD(params, lr=lr, momentum=0.9)
+
+        param_count = np.sum([np.prod(p.size()) for p in params])  # got from MNIST-GAN
+
+        defaults = dict(lr=lr, eps=eps, alpha=alpha,
+                        beta=beta * param_count, gamma=gamma,
+                        sgd_steps=sgd_steps, momentum=momentum, K=K
+                        )
+
+        super(Neumann, self).__init__(params, defaults)
+
+    def step(self, closure=None):
+        """
+        Performs a single optimization step.
+        
+        Arguments:
+            closure (callable, optional): A closure that reevaluates the model
+                and returns the loss.
+        """
+        self.iter += 1
+
+        loss = None
+        if closure is not None:  # checkout what's the deal with this. present in multiple pytorch optimizers
+            loss = closure()
+
+        for group in self.param_groups:
+
+            sgd_steps = group['sgd_steps']
+
+            alpha = group['alpha']
+            beta = group['beta']
+            gamma = group['gamma']
+            K = group['K']
+            momentum = group['momentum']
+            mu = momentum * (1 - (1 / (1 + self.iter)))
+
+            if mu >= 0.9:
+                mu = 0.9
+            elif mu <= 0.5:
+                mu = 0.5
+
+            eta = group['lr'] / self.iter  ## update with time ## changed
+            # print("here")
+
+            for p in group['params']:
+                if p.grad is None:
+                    continue
+                grad = p.grad.data
+
+                state = self.state[p]
+
+                if len(state) == 0:
+                    state['step'] = 0
+                    state['m'] = torch.zeros_like(p.data).float()
+                    state['d'] = torch.zeros_like(p.data).float()
+                    state['moving_avg'] = p.data
+
+                if self.iter <= sgd_steps:
+                    p.data.add_(-group['lr'], grad)
+                    # self.sgd.step()
+                    continue
+
+                state['step'] += 1
+
+                # Reset neumann iterate
+                if self.iter % K == 0:
+                    state['m'] = grad.mul(-eta)
+                    ## changed                  
+
+                else:
+                    ## Compute update d_t
+                    diff = p.data.sub(state['moving_avg'])
+                    # # print(diff)
+                    # diff_norm = p.data.sub(state['moving_avg']).norm()
+                    # if np.count_nonzero(diff) and diff_norm > 0:
+                    #    state['d'] = grad.add( (( (diff_norm.pow(2)).mul(alpha) ).sub( (diff_norm.pow(-2)).mul(beta) )).mul( diff.div(diff_norm)) )
+                    # else:
+                    #    state['d'].add_(grad)
+                    state['d'] = grad
+
+                    ## Update Neumann iterate
+                    (state['m'].mul_(mu)).sub_(state['d'].mul(eta))
+
+                    ## Update Weights
+                    p.data.add_((state['m'].mul(mu)).sub(state['d'].mul(eta)))
+
+                    ## Update Moving Average
+                    # state['moving_avg'] = p.data.add( (state['moving_avg'].sub(p.data)).mul(gamma) )
+
+                # print(p.data)
+
+        ## changed
+        if self.iter % K == 0:
+            group['K'] = group['K'] * 2
+
+        # return loss
---
+++ algorithms/server/server.py
@@ -0,0 +1,338 @@
+import torch
+import os
+
+from algorithms.edges.edgeDONE import edgeDONE
+#from algorithms.edges.edgeSeOrder2 import edgeSeOrder2
+from algorithms.edges.edgeFiOrder import edgeFiOrder
+from algorithms.edges.edgeDANE import edgeDANE
+from algorithms.edges.edgeNew import edgeNew
+from algorithms.edges.edgeGD import edgeGD
+from algorithms.edges.edgeFEDL import edgeFEDL
+from algorithms.edges.edgeNewton import edgeNewton
+from algorithms.edges.edgeAvg import edgeAvg
+from algorithms.edges.edgeGT import edgeGT
+from algorithms.edges.edgePGT import edgePGT
+from algorithms.edges.edgeGIANT import edgeGIANT
+
+from algorithms.server.serverbase import ServerBase
+from utils.model_utils import read_data, read_edge_data
+import numpy as np
+import time
+
+# Implementation for Central Server
+class Server(ServerBase):
+    def __init__(self, experiment, device, dataset, algorithm, model, batch_size, learning_rate, alpha, eta, L, num_glob_iters,
+                 local_epochs, optimizer, num_edges, times):
+        super().__init__(experiment, device, dataset,algorithm, model[0], batch_size, learning_rate, alpha, eta, L, num_glob_iters,
+                         local_epochs, optimizer, num_edges, times)
+
+        # Initialize data for all  edges
+        data = read_data(dataset, read_optimal_weights=False)
+
+        self.optimal_weights = None
+        self.optimal_loss_unreg = None  # Unregularized loss
+        self.optimal_loss_reg = None    # Regularized loss with parameter L
+        if data[-1] is not None:
+            # Synthetic dataset: save the optimal weights for comparison later
+            self.optimal_weights = data[-2]
+            self.optimal_loss_unreg = data[-1]
+            self.optimal_loss_reg = (self.L / 2) * (np.linalg.norm(data[-1]) ** 2)
+
+        total_edges = len(data[0])
+
+        for i in range(total_edges):
+
+            id, train, test = read_edge_data(i, data, dataset)
+
+            if(algorithm == "DONE"):
+                edge = edgeDONE(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "FirstOrder"):
+                edge = edgeFiOrder(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "DANE"):
+                edge = edgeDANE(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if algorithm == "New":
+                edge = edgeNew(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if algorithm == "GD":
+                edge = edgeGD(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+            
+            if algorithm == "FedAvg":
+                edge = edgeAvg(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "FEDL"):
+                edge = edgeDANE(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "Newton"):
+                edge = edgeNewton(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "GT"):
+                edge = edgeGT(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "PGT"):
+                edge = edgePGT(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)
+
+            if(algorithm == "GIANT"):
+                edge = edgeGIANT(device, id, train, test, model, batch_size, learning_rate, alpha, eta, L, local_epochs, optimizer)         
+            
+            self.edges.append(edge)
+            self.total_train_samples += edge.train_samples
+
+        print("Number of edges / total edges:", num_edges, " / ", total_edges)
+        
+    def send_grads(self):
+        assert (self.edges is not None and len(self.edges) > 0)
+        grads = []
+        for param in self.model.parameters():
+            if param.grad is None:
+                grads.append(torch.zeros_like(param.data))
+            else:
+                grads.append(param.grad)
+        for edge in self.edges:
+            edge.set_grads(grads)
+
+    def send_dt(self):
+        for edge in self.edges:
+            edge.set_dt(self.dt)
+
+    def train(self):
+        loss = []
+        start_time = time.time()
+        if(self.algorithm == "FirstOrder"):
+            # All edge will eun GD or SGD to obtain w*
+            for edge in self.edges:
+                edge.train(self.local_epochs)
+            
+            # Communication rounds
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate() # still evaluate on the global model
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+
+                for edge in self.selected_edges:
+                    edge.update_direction()
+                    
+                self.aggregate_parameters()
+
+        elif self.algorithm == "DANE":
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate()
+                # Caculate gradient to send to server for average
+                for edge in self.edges:
+                    edge.get_full_grad()
+                    
+                self.aggregate_grads()
+
+                # receive average gradient form server 
+                self.send_grads()
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs)
+
+                self.aggregate_parameters()          
+
+        elif self.algorithm == "New":
+            for glob_iter in range(1, self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate()
+
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs)
+                self.aggregate_parameters()
+            self.save_results()
+            self.save_model()
+
+        elif self.algorithm == "GD" or self.algorithm == "FedAvg":
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate()
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs, glob_iter)
+                self.aggregate_parameters()
+                
+        elif self.algorithm == "DONE": # Second Order method
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+
+                # recive parameter from server
+                self.send_parameters()
+                self.evaluate()
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                # Caculate gradient to send to server for average
+                for edge in self.selected_edges:
+                    edge.get_full_grad()
+                
+                #self.aggregate_grads()
+                self.aggregate_sub_grads()
+                # receive average gradient form server 
+                self.send_grads()
+               
+                # all note are trained 
+                #self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs, glob_iter)
+
+                self.aggregate_parameters()
+
+        elif self.algorithm == "FEDL":
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.send_grads()
+                self.evaluate()
+
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs)
+                #self.selected_edges[0].train(self.local_epochs)
+                self.aggregate_parameters()
+                self.aggregate_grads()
+
+        elif self.algorithm == "Newton": #using Richardson
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate()
+                # reset all direction after each global interatio
+                # Aggregate grads of client.
+                for edge in self.edges:
+                    edge.get_full_grad()
+                self.aggregate_grads()
+                
+                self.dt = []
+                self.total_dt = []
+                for param in self.model.parameters():
+                    self.dt.append(-param.grad)
+                    self.total_dt.append(torch.zeros_like(param.data))
+
+                # Richardson
+                for r in range(self.local_epochs):
+                    self.send_dt()
+                    self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                    for edge in self.selected_edges:
+                        edge.gethessianproduct(self.local_epochs, glob_iter)
+                    self.aggregate_dt()
+
+                self.aggregate_newton()
+
+        elif self.algorithm == "Newton2": #using inverse hessian
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                self.send_parameters()
+                self.evaluate()
+
+                for edge in self.edges:
+                    edge.get_full_grad()
+                self.aggregate_grads()
+
+                hess = self.aggregate_hessians()
+                inverse_hess = torch.inverse(hess)
+                grads = []
+                for param in self.model.parameters():
+                    grads.append(param.grad.clone().detach())
+                grads_as_vector = torch.cat([-grads[0].data, -grads[1].data.view(1, 1)], 1)
+                direction = torch.matmul(grads_as_vector, inverse_hess)
+                weights_direction = direction[0, 0:-1].view(grads[0].shape)
+                bias_direction = direction[0, -1].view(grads[1].shape)
+
+                for param, d in zip(self.model.parameters(), [weights_direction, bias_direction]):
+                    param.data.add_(self.alpha * d)
+                    
+        elif self.algorithm == "GT" or self.algorithm == "PGT" or self.algorithm == "GIANT":
+            for glob_iter in range(self.num_glob_iters):
+                if(self.experiment):
+                    self.experiment.set_epoch( glob_iter + 1)
+                print("-------------Round number: ",glob_iter, " -------------")
+                # recive parameter from server
+                self.send_parameters()
+                self.evaluate()
+                # Caculate gradient to send to server for average
+                for edge in self.edges:
+                    edge.get_full_grad()
+                
+                self.aggregate_grads()
+                # receive average gradient form server 
+                self.send_grads()
+               
+                # all note are trained 
+                self.selected_edges = self.select_edges(glob_iter, self.num_edges)
+                for edge in self.selected_edges:
+                    edge.train(self.local_epochs, glob_iter)
+
+                self.aggregate_parameters()
+
+        print("--- %s Finish training in seconds ---" % (time.time() - start_time))
+        self.save_results()
+        self.save_model()
+
+    def weights_difference(self, weights=None, optimal_weights=None):
+        """
+        Calculate the norm of w - w*, the difference between the current weights
+        and the optimal weights of the dataset.
+        """
+        if weights is None:
+            weights = list(self.model.parameters())[0].data.clone().detach().flatten().numpy()
+        if optimal_weights is None:
+            optimal_weights = self.optimal_weights
+        if weights.shape != optimal_weights.shape:
+            weights = weights.T
+        return np.linalg.norm(weights - optimal_weights)
+
+    def regularize(self, model=None):
+        model = self.model if model is None else model
+        reg = 0
+        for param in model.parameters():
+            if param.requires_grad:
+                reg += param.norm() ** 2
+        return (self.L / 2) * reg
+
+    def losses_difference(self, loss, optimal_loss=None, regularize=True):
+        """
+        Calculate f(w) - f(w*), the difference between the evaluation function
+        at the current weights and at the optimal weights.
+        """
+        if optimal_loss is None:
+            if regularize:
+                optimal_loss = self.optimal_loss_reg
+            else:
+                optimal_loss = self.optimal_loss_unreg
+        return loss - optimal_loss
+
+    def aggregate_hessians(self):
+        aggregated_hessians = None
+        i = 0
+        total_samples = 0
+        for i, edge in enumerate(self.edges):
+            hess = edge.send_hessian()
+            total_samples += edge.train_samples
+            if aggregated_hessians is None:
+                aggregated_hessians = hess
+            else:
+                aggregated_hessians.add_(hess)
+        return aggregated_hessians / (i + 1 + 1e-6)
